bd0e7a3c178ce8be08ea0cf19baf160755696088
SuperMex <adelinezzm@gmail.com>
2015-02-17 21:23:10 -0500

    edited index.php because a welcome page is required for the checkpoint

--- PhpstormProjects/project1check/index.php
+++ PhpstormProjects/project1check/index.php
@@ -5,5 +5,19 @@
  * Date: 2/12/15
  * Time: 5:03 PM
  */
+?>
 
-echo "index.php";
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Project 1 Checkpoint Home</title>
+</head>
+
+<body>
+Welcome!<br>
+<a href="checkpoint.html">Checkpoint</a>
+</body>
+
+</html>
+
