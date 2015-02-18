14734dbe37592f5fe94aa2515d791af1c21705bc
elhazzat <jhazzat@gmail.com>
2015-02-18 13:15:36 -0500

    Repository Clean up

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elhazzat
+ * Date: 2/12/15
+ * Time: 5:03 PM
+ */
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Project 1 Checkpoint Home</title>
+</head>
+
+<body>
+Welcome!<br>
+<a href="checkpoint.html">Checkpoint</a>
+</body>
+
+</html>
+
