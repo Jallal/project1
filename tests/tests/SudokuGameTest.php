d0518814daa7c20d968b99c2a709e72bd4516afd
SuperMex <adelinezzm@gmail.com>
2015-02-24 12:47:23 -0500

    tests for SudokuModel and SudokuGame

---
+++ tests/tests/SudokuGameTest.php
@@ -0,0 +1,18 @@
+<?php
+/** @file
+ * @cond 
+ * @brief Unit tests for the class SudokuGame
+ */
+class SudokuGameTest extends \PHPUnit_Framework_TestCase
+{
+	public function test_construct() {
+		$sudokuGame = new SudokuGame();
+
+		$this->assertInstanceOf("SudokuGame", $sudokuGame);
+		$this->assertEquals(10, count($sudokuGame->getGames()));
+		$this->assertEquals(10, count($sudokuGame->getAnswers()));
+	}
+}
+
+/// @endcond
+?>
