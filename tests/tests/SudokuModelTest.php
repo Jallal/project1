c88c621c87e773273e0ddea68ddfa554d66b7e63
SuperMex <adelinezzm@gmail.com>
2015-02-23 19:14:33 -0500

    test for getAnsweForCell

--- tests/tests/SudokuModelTest.php
+++ tests/tests/SudokuModelTest.php
@@ -10,6 +10,11 @@ class SudokuModelTest extends \PHPUnit_Framework_TestCase
 		$sudoku = new SudokuModel(1);
 		$this->assertInstanceOf("SudokuModel", $sudoku);
 	}
+
+	public function test_getAnswerForCell() {
+		$sudoku = new SudokuModel(0);
+		$this->assertEquals(8, $sudoku->getAnswerForCell(0,0));
+	}
 }
 
 /// @endcond
